de39aae9eb64c46f6502ab3dad451a32427e1272
unknown <you@example.com>
2022-05-30 23:12:46 -0500

    Se elemina carpeta inecesaria y duplicada

--- app/tiposDocumento/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import TiposDocumento
-
-# Register your models here.
-
-admin.site.register(TiposDocumento)
--- app/tiposLicores/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class TiposLicores(models.Model):
-    idTypeDrink     = models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
-    name            = models.CharField(max_length=30)
